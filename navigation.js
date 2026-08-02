2d1133e30cdac10e4bb675825d6fbdbf563db8dc
numismaticnovelties <numismaticnovelties@icloud.com>
2026-08-02 16:51:27 -0500

    Attempt 2: Navigation loading script

---
+++ navigation.js
@@ -0,0 +1,27 @@
+let data;
+let ready = false;
+            fetch('https://numismaticnovelties.github.io/header.html')
+            .then(response => {
+                if (!response.ok) {
+                    console.error(`HTTP error! status: ${response.status}`);
+                }
+                // Check if response has content
+                return response.text(); // Get as text first
+                })
+            .then(text => {
+            if (!text) {
+            console.error('Empty response received');
+            return;
+            }
+            display(text);
+            })
+            .catch(error => console.error('Fetch error:', error));
+            function display(fetchedData){
+                data = fetchedData;
+                ready=true;
+            }
+setInterval(()=>{
+    if (ready){
+        document.getElementById('nav').innerHTML = data;
+    }
+})
